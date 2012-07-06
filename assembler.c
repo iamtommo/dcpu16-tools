d8b94e1e22d3f97857106e49e323b0885957dd9d
Thomas Madsen <iamtommo@gmail.com>
2012-07-06 17:17:25 +0200

    Value-at-address/bracketed values now work.

--- assembler.c
+++ assembler.c
@@ -6,7 +6,7 @@
 #include <stdint.h>
 
 const char *token_delimeters = " ,\t\n";
-char input[] = "SET A, 0x69\nSET B, 0x68\nADD A, 0x01";
+char input[] = "SET A, 0x69\nSET B, 0x01\nSET [0x5000+B], B";
 char* token = NULL;//current token
 uint16_t PC = 0;//program counter
 uint16_t assembled[65535];//the assembled result
@@ -65,6 +65,19 @@ int hexstr_to_int(char* str) {
 	return atoi(c);
 }
 
+char* remove_brackets(char* src) {
+	char *c = (char*) malloc(strlen(src) - 2);
+	strncpy(c, src+1, strlen(src)-2);
+	return c;
+}
+
+void tolowercase(char *str) {
+	int i;
+	for (i = 0; i < strlen(str); i++) {
+		str[i] = tolower(str[i]);
+	}
+}
+
 int assemble_operand() {
 	next_token();
 
@@ -84,7 +97,6 @@ int assemble_operand() {
 		return 0x06;
 	} else if (strcmp(token, "j") == 0) {
 		return 0x07;
-	//TODO: Bracketed registers
 	} else if (strcmp(token, "pop") == 0) {
 		return 0x18;
 	} else if (strcmp(token, "peek") == 0) {
@@ -108,15 +120,92 @@ int assemble_operand() {
 			assembled[PC++] = n;
 			return 0x1f;
 		} else {
-			//must be bracketed []
-			//TODO: Bracketed values
+			//must be a bracketed value
+			if (token[0] != '[') {
+				die(strcat("Expected bracketed value, instead got ", token));
+			}
+			printf("Analyzing bracketed token: %s\n", token);
+
+			//remove the brackets from the token
+			char *token2 = remove_brackets(token);
+			printf("%s > %s\n", token, token2);
+			//split it into subtokens
+			char* saveptr;
+			char* subtoken = strtok_r(token2, "+", &saveptr);
+			tolowercase(subtoken);
+			//TODO: Labels inside bracketed tokens
+			if (is_string(subtoken)) {
+				printf("%s is string\n", subtoken);
+				uint16_t val = 0;
+				if (strcmp(subtoken, "a") == 0) {
+					val = 0x08;
+				} else if (strcmp(subtoken, "b") == 0) {
+					val = 0x09;
+				} else if (strcmp(subtoken, "c") == 0) {
+					val = 0x0a;
+				} else if (strcmp(subtoken, "x") == 0) {
+					val = 0x0b;
+				} else if (strcmp(subtoken, "y") == 0) {
+					val = 0x0c;
+				} else if (strcmp(subtoken, "z") == 0) {
+					val = 0x0d;
+				} else if (strcmp(subtoken, "i") == 0) {
+					val = 0x0e;
+				} else if (strcmp(subtoken, "j") == 0) {
+					val = 0x0f;
+				}
+
+				subtoken = strtok_r(NULL, "+", &saveptr);
+				if (subtoken != NULL) {
+					tolowercase(subtoken);
+					if (is_number(subtoken)) {
+						assembled[PC++] = hexstr_to_int(subtoken);
+						return val;
+					} else if (is_string(subtoken)) {
+						//TODO
+					} else {
+						die(strcat("Expected real value, instead got: ", subtoken));
+					}
+				}
+			} else if (is_number(subtoken)) {
+				printf("%s is number\n", subtoken);
+				assembled[PC++] = hexstr_to_int(subtoken);
+				subtoken = strtok_r(NULL, "+", &saveptr);
+				if (subtoken != NULL) {
+					tolowercase(subtoken);
+					if (strcmp(subtoken, "a") == 0) {
+						return 0x10;
+					} else if (strcmp(subtoken, "b") == 0) {
+						return 0x11;
+					} else if (strcmp(subtoken, "c") == 0) {
+						return 0x12;
+					} else if (strcmp(subtoken, "x") == 0) {
+						return 0x13;
+					} else if (strcmp(subtoken, "y") == 0) {
+						return 0x14;
+					} else if (strcmp(subtoken, "z") == 0) {
+						return 0x15;
+					} else if (strcmp(subtoken, "i") == 0) {
+						return 0x16;
+					} else if (strcmp(subtoken, "j") == 0) {
+						return 0x17;
+					} else {
+						die(strcat("Invalid register: ", subtoken));
+					}
+				} else {
+					return 0x1e;//next word
+				}
+			} else {
+				die(strcat("Malformed operand: ", subtoken));
+			}
+			//free(token2);
 		}
 	}
 
 	return 0;
 }
 
-int get_opcode(char *op) {
+uint16_t get_opcode(char *op) {
 	if (strcmp(op, "set") == 0) {
 		return 0x1;
 	} else if (strcmp(op, "add") == 0) {
@@ -158,15 +247,15 @@ void assemble() {
 	int a, b;
 	char* op = token;
 
-	if (token == "push") {
+	if (strcmp(token, "push") == 0) {
 		op = "set";
 		a = 0x1a;//push
 		b = assemble_operand();
-	} else if (token == "pop") {
+	} else if (strcmp(token, "pop") == 0) {
 		op = "set";
 		a = assemble_operand();
 		b = 0x18;//pop
-	} else if (token == "nop") {
+	} else if (strcmp(token, "nop") == 0) {
 		op = "set";
 		a = 0x20;
 		b = 0x20;
@@ -175,22 +264,24 @@ void assemble() {
 		b = assemble_operand();
 	}
 
-	int opcode = get_opcode(op);
-	printf("Opcode '%s' > '%x'\n", op, opcode);
-	printf("o[%x] a[%x] b[%x]\n", opcode, a, b);
-	assembled[_PC] = opcode | (a << 4) | (b << 10);
+	uint16_t opcode = get_opcode(op);
+	printf("o=%x a=%x b=%x\n", opcode, a, b);
+	assembled[_PC] = opcode;//lower 4 bits
+	assembled[_PC] |= (a << 4);//lower 6 of 12 bits
+	assembled[_PC] |= (b << 10);//higher 6 of 12 bits
 }
 
 int main(int argc, char **argv) {
+	printf("-----------------------------\n");
 	for(;;) {
 		next_token();
 
 		if (token == NULL) {
-			printf("%d instructions assembled, pc = %d\n", total_instructions, PC);
-			printf("Output: \n");
-			int i = 0;
-			for (i; i < PC; i++) {
-				printf("value: 0x%d\n", assembled[i]);
+			printf("-----------------------------\n%d instructions assembled, pc = %d\n", total_instructions, PC);
+			printf("Assembled data: \n");
+			int i;
+			for (i = 0; i < PC; i++) {
+				printf("\tvalue: 0x%x\n", assembled[i]);
 			}
 			die("Null token reached!");
 			break;
